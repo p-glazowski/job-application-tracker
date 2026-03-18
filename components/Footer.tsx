2cc20874db75180aee31d37aca3d1d57e04e47e4
Piotr <hunter976@gmail.com>
2026-03-18 18:50:19 +0100

    created footer

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  const date = new Date();
+  console.log(date.getFullYear);
+  return (
+    <footer className="flex items-center justify-center p-1 bg-pink-500 text-sm text-white font-bold mt-40">
+      <h4>
+        Created by Piotr Głazowski{' '}
+        <span className="ml-0">&copy; {date.getFullYear()}</span>
+      </h4>
+    </footer>
+  );
+}
