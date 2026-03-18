218d6c717ae5ee2a6fd8fe8af74ac708bc2e6b2d
Piotr <hunter976@gmail.com>
2026-03-18 18:50:29 +0100

    updated home page with sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import { auth } from '@/auth';
+import Link from 'next/link';
+import ScreenSection from '@/components/homePage/ScreenSection';
+import PerksSection from '@/components/homePage/PerksSection';
+import Footer from '@/components/Footer';
+
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div className="flex-1">
       <section className="my-20 text-center max-w-200 mx-auto grid gap-4">
@@ -10,29 +18,22 @@ export default function Home() {
           Capture, organize and manage your job search in one place.
         </p>
         <div className="mt-5">
-          <button className="bg-pink-500 rounded-md p-2 px-6 text-white font-bold cursor-pointer hover:bg-pink-600">
+          <Link
+            href={session ? '/dashboard' : '/login'}
+            className="bg-pink-500 rounded-md p-2 px-6 text-white font-bold cursor-pointer hover:bg-pink-600"
+          >
             Start right now!
-          </button>
+          </Link>
         </div>
         <p className="text-gray-400 text-xs">
           Free forever. No credit card required.
         </p>
       </section>
       <div className="border-b-2 border-black/10"></div>
-      <section className="flex flex-col items-center my-20">
-        <div className="flex gap-5">
-          <button className="bg-gray-400 text-white p-1 px-4 rounded-md bg-pink-500 hover:opacity-80">
-            Organize applications
-          </button>
-          <button className="bg-gray-400 text-white p-1 px-4 rounded-md hover:opacity-80">
-            Get hired
-          </button>
-          <button className="bg-gray-400 text-white p-1 px-4 rounded-md hover:opacity-80">
-            Manage boards
-          </button>
-        </div>
-      </section>
+      <ScreenSection />
       <div className="border-b-2 border-black/10"></div>
+      <PerksSection />
+      <Footer />
     </div>
   );
 }
